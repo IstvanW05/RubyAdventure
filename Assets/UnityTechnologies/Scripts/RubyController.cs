ca8a069f4a35ab7a34049b759de47663e0c125b5
Istvan <32124flame@gmail.com>
2024-04-19 13:52:12 -0400

    :3

---
+++ Assets/UnityTechnologies/Scripts/RubyController.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RubyController : MonoBehaviour
+{
+    // ========= MOVEMENT =================
+    public float speed = 4;
+    
+    // ======== HEALTH ==========
+    public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+    public ParticleSystem hitParticle;
+    public ParticleSystem damageParticle;
+    public ParticleSystem healthParticle;
+    
+    // ======== PROJECTILE ==========
+    public GameObject projectilePrefab;
+
+    // ======== AUDIO ==========
+    public AudioClip hitSound;
+    public AudioClip shootingSound;
+    
+    // ======== HEALTH ==========
+    public int health
+    {
+        get { return currentHealth; }
+    }
+    
+    // =========== MOVEMENT ==============
+    Rigidbody2D rigidbody2d;
+    Vector2 currentInput;
+    
+    // ======== HEALTH ==========
+    int currentHealth;
+    float invincibleTimer;
+    bool isInvincible;
+   
+    // ==== ANIMATION =====
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+    
+    // ================= SOUNDS =======================
+    AudioSource audioSource;
+
+    // ================= UI =================
+    public GameObject gameOverText;
+    public GameObject youWinText;
+    bool gameOver = false;
+
+    // ========= Score =========
+    public int score = 0;
+    // public Text scoreText;
+    // public GameObject ScoreText; 
+
+    void Start()
+    {
+        // =========== MOVEMENT ==============
+        rigidbody2d = GetComponent<Rigidbody2D>();
+                
+        // ======== HEALTH ==========
+        invincibleTimer = -1.0f;
+        currentHealth = maxHealth;
+        
+        // ==== ANIMATION =====
+        animator = GetComponent<Animator>();
+        
+        // ==== AUDIO =====
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        // ================= HEALTH ====================
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+
+        // ============== MOVEMENT ======================
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+                
+        Vector2 move = new Vector2(horizontal, vertical);
+        
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        currentInput = move;
+        if (Input.GetKeyDown(KeyCode.R))
+                {
+                    if (gameOver == true)
+                    {
+                        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                        print("World reset!");
+                    }
+                }
+
+
+        // ============== ANIMATION =======================
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        // ============== PROJECTILE ======================
+
+        if (Input.GetKeyDown(KeyCode.C))
+            LaunchProjectile();
+        
+        // ======== DIALOGUE ==========
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, 1 << LayerMask.NameToLayer("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }  
+            }
+        }
+ 
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        
+        position = position + currentInput * speed * Time.deltaTime;
+        
+        rigidbody2d.MovePosition(position);
+    }
+
+    // ===================== HEALTH ==================
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        { 
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+            
+            animator.SetTrigger("Hit");
+            audioSource.PlayOneShot(hitSound);
+
+            Instantiate(hitParticle, transform.position + Vector3.up * 0.5f, Quaternion.identity);
+            Instantiate(damageParticle, transform.position + Vector3.up * 0.5f, Quaternion.identity);
+        }
+        if (amount > 0)
+        {
+            Instantiate(healthParticle, transform.position + Vector3.up * 0.5f, Quaternion.identity);
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        
+        if(currentHealth == 0)
+          {
+            gameOver = true;
+
+            speed = 0.0f;
+            print("Game Over!");
+
+            gameOverText.SetActive(true);
+          }
+        UIHealthBar.Instance.SetValue(currentHealth / (float)maxHealth);
+    }
+    
+    
+    // =============== PROJECTICLE ========================
+    void LaunchProjectile()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+        
+        animator.SetTrigger("Launch");
+        audioSource.PlayOneShot(shootingSound);
+    }
+    
+    // =============== SOUND ==========================
+
+    //Allow to play a sound on the player sound source. used by Collectible
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+
+    // ==================== Score ====================
+    public void ChangeScore (int scoreAmount)
+    {
+         if (scoreAmount > 0)
+        {
+            score = score + scoreAmount;
+            print ("The current score is " + score);
+        }
+        
+       // scoreText.text = "Fixed Robots: " + score.ToString(); // This is the code that breaks the game
+       
+        if (score == 4)
+        {
+            youWinText.SetActive(true);
+
+            gameOver = true;
+
+            speed = 0.0f;
+            print("Game Over!");
+        }
+    }
+}
